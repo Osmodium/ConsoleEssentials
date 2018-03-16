bb8454d9263d84a0d1035f4d39c9cc26e6a76e7a
Christian Schubert <schubertchr@gmail.com>
2018-03-16 15:46:38 +0100

    Inital commit

---
+++ ConsoleEssentials/Arguments.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Linq;
+
+namespace ConsoleEssentials
+{
+    /// <summary>
+    /// Used to handle arguments from a console in a consistent way.
+    /// </summary>
+    public static class Arguments
+    {
+        /// <summary>
+        /// Parse all the argument strings to the interpreter to generate the options hashtable
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns>Options hashtable</returns>
+        public static Hashtable GetOptions(string[] args)
+        {
+            Hashtable options = new Hashtable();
+            for (int i = 0; i < args.Length; ++i)
+            {
+                string arg = args[i];
+                if (!arg[0].Equals('-'))
+                    continue;
+                int j = i + 1;
+                if (j >= args.Length || args[j][0].Equals('-'))
+                    options.Add(arg.TrimStart('-').ToLower(), true);
+                else if (i < args.Length)
+                {
+                    options.Add(arg.TrimStart('-').ToLower(), args[j]);
+                    ++i;
+                }
+            }
+            return options;
+        }
+
+        /// <summary>
+        /// Verifies that the options required is indeed present in the options table.
+        /// </summary>
+        /// <param name="hashtable">The options hashtable containing the options.</param>
+        /// <param name="requiredOptions">Array containing the names of all the required opions.</param>
+        /// <returns>Array of all the missing required options.</returns>
+        public static string[] CheckOptions(this Hashtable hashtable, string[] requiredOptions)
+        {
+            return requiredOptions.Where(requiredOption => !hashtable.ContainsKey(requiredOption.ToLower())).ToArray();
+        }
+
+        /// <summary>
+        /// Gets the string value of the option.
+        /// </summary>
+        /// <param name="hashtable">The options hashtable containing the options.</param>
+        /// <param name="name">Name of the option to get the string value of.</param>
+        /// <returns>The string value of the named option.</returns>
+        public static string GetOptionString(this Hashtable hashtable, string name)
+        {
+            object option = hashtable.GetOptionObject(name);
+            return (string)option;
+        }
+
+        /// <summary>
+        /// Gets the string value of the option if it is not null or empty, otherwise the original value is returned.
+        /// </summary>
+        /// <param name="hashtable"></param>
+        /// <param name="originalValue"></param>
+        /// <param name="name"></param>
+        /// <returns>If the option does exists and has a value it gets returned, otherwise the original value is returned.</returns>
+        public static string GetOptionStringIfNotNull(this Hashtable hashtable, string originalValue, string name)
+        {
+            string optionString = hashtable.GetOptionString(name);
+            return !string.IsNullOrEmpty(optionString) ? optionString : originalValue;
+        }
+
+        /// <summary>
+        /// Returns true if the option switch is present, false otherwise.
+        /// </summary>
+        /// <param name="hashtable">The options hashtable containing the options.</param>
+        /// <param name="name">Name of the switch option.</param>
+        /// <returns>True or false if the option name is present.</returns>
+        public static bool GetOptionSwitch(this Hashtable hashtable, string name)
+        {
+            object option = hashtable.GetOptionObject(name);
+            return option != null;
+        }
+
+        /// <summary>
+        /// Checks if the option with a name is in the argument collection
+        /// </summary>
+        /// <param name="hashtable"></param>
+        /// <param name="name"></param>
+        /// <returns>True or false weather an option with the name exists in the options collection.</returns>
+        public static bool ContainsOption(this Hashtable hashtable, string name)
+        {
+            return hashtable.ContainsKey(name.ToLowerInvariant());
+        }
+
+        private static object GetOptionObject(this Hashtable hashtable, string name)
+        {
+            return hashtable[name.ToLowerInvariant()];
+        }
+    }
+}
---
+++ ConsoleEssentials/Log.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleEssentials
+{
+    /// <summary>
+    /// Logging for console and text files.
+    /// </summary>
+    public static class Log
+    {
+        #region Properties
+
+        private static bool m_LogToConsole = true;
+        private static bool m_LogToFile = true;
+        private static readonly string m_DefaultLogPath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "Log");
+        private static string m_LogFileName = string.Empty;
+        private static string m_LogPath = m_DefaultLogPath;
+
+        /// <summary>
+        /// Set the log to text file path to a different one than the one that gets created next to the executeable.
+        /// </summary>
+        public static string LogPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(m_LogPath))
+                    m_LogPath = m_DefaultLogPath;
+                return m_LogPath;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    m_LogPath = value;
+            }
+        }
+
+        /// <summary>
+        /// Turn logging to text files on/off
+        /// </summary>
+        /// <param name="value"></param>
+        public static void SetLogToFile(bool value)
+        {
+            m_LogToFile = value;
+        }
+
+        /// <summary>
+        /// Turn logging to the console on/off
+        /// </summary>
+        /// <param name="value"></param>
+        public static void SetLogToConsole(bool value)
+        {
+            m_LogToConsole = value;
+        }
+
+        private static string m_LogDateTime;
+        /// <summary>
+        /// Gets the date time of the log in a file valid format.
+        /// </summary>
+        public static string LogDateTime
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(m_LogDateTime))
+                    return m_LogDateTime;
+                DateTime now = DateTime.Now;
+                m_LogDateTime = $"{now.Year}{now.Month.ToString().PadLeft(2, '0')}{now.Day.ToString().PadLeft(2, '0')}_{now.Hour.ToString().PadLeft(2, '0')}{now.Minute.ToString().PadLeft(2, '0')}{now.Second.ToString().PadLeft(2, '0')}";
+                return m_LogDateTime;
+            }
+        }
+
+        #endregion
+
+        private static void WriteLog(string message, string type)
+        {
+            string logMessage = $"{DateTime.Now} - {type} - {message}";
+            if (m_LogToConsole)
+                Console.WriteLine(logMessage);
+
+            if (!m_LogToFile)
+                return;
+
+            if (!Directory.Exists(m_LogPath))
+                Directory.CreateDirectory(m_LogPath);
+            if (string.IsNullOrEmpty(m_LogFileName))
+            {
+                m_LogFileName = $"Log{LogDateTime}.txt";
+            }
+            using (StreamWriter sw = File.AppendText(Path.Combine(m_LogPath, m_LogFileName)))
+            {
+                sw.WriteLine(logMessage);
+            }
+        }
+
+        #region Information
+
+        /// <summary>
+        /// Log an information message (INFO)
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Information(string message)
+        {
+            WriteLog(message, "INFO ");
+        }
+
+        /// <summary>
+        /// Log an exception as an information message (INFO)
+        /// </summary>
+        /// <param name="ex"></param>
+        public static void Information(Exception ex)
+        {
+#if NET4_5 || NET4_5_1 || NET4_5_2 || NET4_6 || NET4_6_1
+            Information($"{ex.Message}[{ex.HResult}]{ex.StackTrace}");
+#else
+            Information($"{ex.Message}{ex.StackTrace}");
+#endif
+        }
+
+        /// <summary>
+        /// Log both a message and an exception as an information message (INFO)
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="ex"></param>
+        public static void Information(string message, Exception ex)
+        {
+#if NET4_5 || NET4_5_1 || NET4_5_2 || NET4_6 || NET4_6_1
+            Information($"{message}{Environment.NewLine}{ex.Message}[{ex.HResult}]{ex.StackTrace}");
+#else
+            Information($"{message}{Environment.NewLine}{ex.Message}{ex.StackTrace}");
+#endif
+        }
+
+        #endregion
+
+        #region Warning
+
+        /// <summary>
+        /// Log a warning message (WARN)
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Warning(string message)
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            WriteLog(message, "WARN ");
+            Console.ForegroundColor = defaultColor;
+        }
+
+        /// <summary>
+        /// Log an exception as a warning message (WARN)
+        /// </summary>
+        /// <param name="ex"></param>
+        public static void Warning(Exception ex)
+        {
+#if NET4_5 || NET4_5_1 || NET4_5_2 || NET4_6 || NET4_6_1
+            Warning($"{ex.Message}[{ex.HResult}]{ex.StackTrace}");
+#else
+            Warning($"{ex.Message}{ex.StackTrace}{Environment.NewLine}");
+#endif
+        }
+
+        /// <summary>
+        /// Log both a message and an exception as a warning message (WARN)
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="ex"></param>
+        public static void Warning(string message, Exception ex)
+        {
+#if NET4_5 || NET4_5_1 || NET4_5_2 || NET4_6 || NET4_6_1
+            Warning($"{message}{Environment.NewLine}{ex.Message}[{ex.HResult}]{ex.StackTrace}");
+#else
+            Warning($"{message}{Environment.NewLine}{ex.Message}{ex.StackTrace}");
+#endif
+        }
+
+        #endregion
+
+        #region Error
+
+        /// <summary>
+        /// Log an error message (ERROR)
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Error(string message)
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            WriteLog(message, "ERROR");
+            Console.ForegroundColor = defaultColor;
+        }
+
+        /// <summary>
+        /// Log an exception as an error message (ERROR)
+        /// </summary>
+        /// <param name="ex"></param>
+        public static void Error(Exception ex)
+        {
+#if NET4_5 || NET4_5_1 || NET4_5_2 || NET4_6 || NET4_6_1
+            Error($"{ex.Message}[{ex.HResult}]{ex.StackTrace}");
+#else
+            Error($"{ex.Message}{ex.StackTrace}");
+#endif
+        }
+
+        /// <summary>
+        /// Log both a message and an exception as an error message (ERROR)
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="ex"></param>
+        public static void Error(string message, Exception ex)
+        {
+#if NET4_5 || NET4_5_1 || NET4_5_2 || NET4_6 || NET4_6_1
+            Error($"{message}{Environment.NewLine}{ex.Message}[{ex.HResult}]{ex.StackTrace}");
+#else
+            Error($"{message}{Environment.NewLine}{ex.Message}{ex.StackTrace}");
+#endif
+        }
+
+        #endregion
+
+        #region Critical
+
+        /// <summary>
+        /// Log a critical message (CRIT)
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Critical(string message)
+        {
+            ConsoleColor defaultForegroundColor = Console.ForegroundColor;
+            ConsoleColor defaultBackgroundColor = Console.BackgroundColor;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Red;
+            WriteLog(message, "CRIT ");
+            Console.ForegroundColor = defaultForegroundColor;
+            Console.BackgroundColor = defaultBackgroundColor;
+        }
+
+        /// <summary>
+        /// Log an exception as a critical message (CRIT)
+        /// </summary>
+        /// <param name="ex"></param>
+        public static void Critical(Exception ex)
+        {
+#if NET4_5 || NET4_5_1 || NET4_5_2 || NET4_6 || NET4_6_1
+            Critical($"{ex.Message}[{ex.HResult}]{ex.StackTrace}");
+#else
+            Critical($"{ex.Message}{ex.StackTrace}");
+#endif
+        }
+
+        /// <summary>
+        /// Log both a message and an exception as a critical message (CRIT)
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="ex"></param>
+        public static void Critical(string message, Exception ex)
+        {
+#if NET4_5 || NET4_5_1 || NET4_5_2 || NET4_6 || NET4_6_1
+            Critical($"{message}{Environment.NewLine}{ex.Message}[{ex.HResult}]{ex.StackTrace}");
+#else
+            Critical($"{message}{Environment.NewLine}{ex.Message}{ex.StackTrace}");
+#endif
+        }
+
+        #endregion
+    }
+}
